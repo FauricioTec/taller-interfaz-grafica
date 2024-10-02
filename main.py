db965bb6fe2cad12f7e353570239370e68614cee
FauricioTec <fagranados@estudiantec.cr>
2024-10-02 11:35:02 -0600

    paso 2: la primera ventana

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+#pip install tk
+
+root = tk.Tk() # Crea la ventana principal
+
+root.title('Hola Mundo') # Titulo de la ventana
+root.geometry('800x550')
+root.resizable(False, False)
+
+root.mainloop() # Mantiene la ventana abierta
