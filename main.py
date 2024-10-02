5ad6005512f008ce53c87df79e6e98856e31297a
FauricioTec <fagranados@estudiantec.cr>
2024-10-02 15:54:55 -0600

    paso 5 (opciona): definir los colores a utilizar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import LEFT, RIGHT, BOTH
+from tkinter import LEFT, RIGHT, BOTH, TOP
 #pip install tk
 
 root = tk.Tk() # Crea la ventana principal
@@ -14,14 +14,24 @@ yPosicion = root.winfo_screenheight() // 2 - altoVentana // 2
 yPosicion -= 20 # Correccion para la barra de tareas
 
 root.geometry("%dx%d+%d+%d" % (anchoVentana, altoVentana, xPosicion, yPosicion))
-root.resizable(False, False) # Este *metodo* evita que la ventana se pueda redimensionar
+#root.resizable(False, False) # Este *metodo* evita que la ventana se pueda redimensionar
 
-# Se empieza a agregar widgets
+colores = {
+    'naranja_palido': '#fff8f0', 
+    'negro': '#1e1e24', 
+    'negro_azulado': '#14141b', 
+    'azul_grisaceo_claro': '#0a0a96', 
+    'blanco_azulado': '#dee4f7', 
+    'azul_oscuro': '#0b1432', 
+    'azul_grisaceo_claro': '#eef1fb', 
+    'naranja_grisaceo': '#d7d0c8', 
+    'naranja_claro': '#ffd099'
+}
 
-contenedorLateral = tk.Frame(root, bg='blue', width=anchoVentana / 3, height=altoVentana)
-contenedorLateral.pack(side=LEFT)
+contenedorLateral = tk.Frame(root, bg=colores['negro'], width=anchoVentana / 3, height=altoVentana)
+contenedorLateral.pack(side=LEFT, fill='y')
 
-contenedorPrincipal = tk.Frame(root, bg='red')
+contenedorPrincipal = tk.Frame(root, bg=colores['naranja_palido'])
 contenedorPrincipal.pack(side=RIGHT, fill=BOTH, expand=True)
 
 # Notese que el root.mainloop() debe ser lo ultimo que se ejecute
