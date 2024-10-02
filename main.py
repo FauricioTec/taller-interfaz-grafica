499408e4011047f14ec7650a310e1f35baf7a6f1
FauricioTec <fagranados@estudiantec.cr>
2024-10-02 12:18:13 -0600

    paso 4: crear los contenedores

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import LEFT, RIGHT, BOTH
 #pip install tk
 
 root = tk.Tk() # Crea la ventana principal
@@ -15,4 +16,13 @@ yPosicion -= 20 # Correccion para la barra de tareas
 root.geometry("%dx%d+%d+%d" % (anchoVentana, altoVentana, xPosicion, yPosicion))
 root.resizable(False, False) # Este *metodo* evita que la ventana se pueda redimensionar
 
+# Se empieza a agregar widgets
+
+contenedorLateral = tk.Frame(root, bg='blue', width=anchoVentana / 3, height=altoVentana)
+contenedorLateral.pack(side=LEFT)
+
+contenedorPrincipal = tk.Frame(root, bg='red')
+contenedorPrincipal.pack(side=RIGHT, fill=BOTH, expand=True)
+
+# Notese que el root.mainloop() debe ser lo ultimo que se ejecute
 root.mainloop() # Mantiene la ventana abierta
