b7c2b0a4b27fd78dbec72b639cc74e24a36fe2b9
FauricioTec <fagranados@estudiantec.cr>
2024-10-02 16:24:04 -0600

    paso 6: creamos el primer label

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import tkinter as tk
-from tkinter import LEFT, RIGHT, BOTH, TOP
-#pip install tk
+from tkinter.font import Font
+from tkinter import LEFT, RIGHT, BOTH, Y, CENTER
 
 root = tk.Tk() # Crea la ventana principal
 
 root.title('Hola Mundo') # Titulo de la ventana
 
-altoVentana = 500
-anchoVentana = 800
+altoVentana = 600
+anchoVentana = 900
 
 xPosicion = root.winfo_screenwidth() // 2 - anchoVentana // 2
 yPosicion = root.winfo_screenheight() // 2 - altoVentana // 2
-yPosicion -= 20 # Correccion para la barra de tareas
+yPosicion -= 40 # Correccion para la barra de tareas
 
 root.geometry("%dx%d+%d+%d" % (anchoVentana, altoVentana, xPosicion, yPosicion))
-#root.resizable(False, False) # Este *metodo* evita que la ventana se pueda redimensionar
+root.resizable(False, False) # Este *metodo* evita que la ventana se pueda redimensionar
 
 colores = {
     'naranja_palido': '#fff8f0', 
@@ -28,11 +28,18 @@ colores = {
     'naranja_claro': '#ffd099'
 }
 
-contenedorLateral = tk.Frame(root, bg=colores['negro'], width=anchoVentana / 3, height=altoVentana)
-contenedorLateral.pack(side=LEFT, fill='y')
+contenedorLateral = tk.Frame(root, bg=colores['negro'], width=anchoVentana / 3.5, height=altoVentana)
+contenedorLateral.pack(side=LEFT, fill=Y)
 
 contenedorPrincipal = tk.Frame(root, bg=colores['naranja_palido'])
 contenedorPrincipal.pack(side=RIGHT, fill=BOTH, expand=True)
+
+# Definimos la fuente
+fuente_segoe = Font(family="Segoe UI", size=17, weight='bold', slant="roman")
+
+# Colocamos el primer Label
+tituloBarraLateral = tk.Label(contenedorLateral, text='¡Consulta tu tabla de pagos!', bg=colores['negro'], fg='white', font=fuente_segoe, wraplength=200)
+tituloBarraLateral.place(anchor=CENTER, relx=.5, rely=.15)  #Se usa el anchos y el relx para colocar el label en el medio
 
 # Notese que el root.mainloop() debe ser lo ultimo que se ejecute
 root.mainloop() # Mantiene la ventana abierta
