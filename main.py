30b4becbf3ec0f696150f14a1f2bc13a8b2faa67
FauricioTec <fagranados@estudiantec.cr>
2024-10-02 11:46:29 -0600

    paso 3: centrar la ventana

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ import tkinter as tk
 root = tk.Tk() # Crea la ventana principal
 
 root.title('Hola Mundo') # Titulo de la ventana
-root.geometry('800x550')
-root.resizable(False, False)
+
+altoVentana = 500
+anchoVentana = 800
+
+xPosicion = root.winfo_screenwidth() // 2 - anchoVentana // 2
+yPosicion = root.winfo_screenheight() // 2 - altoVentana // 2
+yPosicion -= 20 # Correccion para la barra de tareas
+
+root.geometry("%dx%d+%d+%d" % (anchoVentana, altoVentana, xPosicion, yPosicion))
+root.resizable(False, False) # Este *metodo* evita que la ventana se pueda redimensionar
 
 root.mainloop() # Mantiene la ventana abierta
