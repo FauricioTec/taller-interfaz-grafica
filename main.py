70eafd92bcd21e19b854ebd31a039bf8218a9955
FauricioTec <fagranados@estudiantec.cr>
2024-10-02 19:42:37 -0600

    paso 7: agregamos los widgets entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter.font import Font
-from tkinter import LEFT, RIGHT, BOTH, Y, CENTER
+from tkinter import LEFT, RIGHT, BOTH, Y, CENTER, W, E, N, S
 
 root = tk.Tk() # Crea la ventana principal
 
@@ -35,11 +35,39 @@ contenedorPrincipal = tk.Frame(root, bg=colores['naranja_palido'])
 contenedorPrincipal.pack(side=RIGHT, fill=BOTH, expand=True)
 
 # Definimos la fuente
-fuente_segoe = Font(family="Segoe UI Semibold", size=17, slant="roman")
+fuenteTituloLateral = Font(family="Segoe UI Semibold", size=17, slant="roman")
 
 # Colocamos el primer Label
-tituloBarraLateral = tk.Label(contenedorLateral, text='¡Consulta tu tabla de pagos!', bg=colores['negro'], fg='white', font=fuente_segoe, wraplength=200)
+tituloBarraLateral = tk.Label(contenedorLateral, text='¡Consulta tu tabla de pagos!', bg=colores['negro'], fg='white', font=fuenteTituloLateral, wraplength=200)
 tituloBarraLateral.place(anchor=CENTER, relx=.5, rely=.15)  #Se usa el anchor center y el relx para colocar el label en el medio
+# Definimos las fuentes para los entrys
+fuenteEntry = Font(family="Segoe UI", size=12)
+fuenteLabelEntry = Font(family="Segoe UI", size=8, weight='bold')
+
+# Label y Entry para Nombre
+labelInputNombre = tk.Label(contenedorLateral, text="Nombre del cliente", bg=colores['negro'], fg='white', font=fuenteLabelEntry)
+labelInputNombre.place(anchor=W, relx=.14, rely=.25) # Tener cuidado a la hora de usar posiciones relaticas y definir anchos y altos a la vez
+inputNombre = tk.Entry(contenedorLateral, bg='white', fg='black', font=fuenteEntry, width=20, bd=2, relief="solid")
+inputNombre.place(anchor='center', relx=.5, rely=.3)
+
+# Label y Entry para Monto
+labelInputMonto = tk.Label(contenedorLateral, text="Monto del prestamo", bg=colores['negro'], fg='white', font=fuenteLabelEntry)
+labelInputMonto.place(anchor=W, relx=.14, rely=.35)
+inputMonto = tk.Entry(contenedorLateral, bg='white', fg='black', font=fuenteEntry, width=20, bd=2, relief="solid")
+inputMonto.place(anchor=CENTER, relx=.5, rely=.4)
+
+# Label y Entry para Plazo
+labelInputPlazo = tk.Label(contenedorLateral, text="Plazo en años", bg=colores['negro'], fg='white', font=fuenteLabelEntry)
+labelInputPlazo.place(anchor=W, relx=0.14, rely=.45)
+inputPlazo = tk.Entry(contenedorLateral, bg='white', fg='black', font=fuenteEntry, width=20, bd=2, relief="solid")
+inputPlazo.place(anchor=CENTER, relx=.5, rely=.5)
+
+# Label y Entry para Interés
+labelInputInteres = tk.Label(contenedorLateral, text="Interés anual %", bg=colores['negro'], fg='white', font=fuenteLabelEntry)
+labelInputInteres.place(anchor=W, relx=.14, rely=.55)
+inputInteres = tk.Entry(contenedorLateral, bg='white', fg='black', font=fuenteEntry, width=20, bd=2, relief="solid")
+inputInteres.place(anchor=CENTER, relx=.5, rely=.6)
+
 
 # Notese que el root.mainloop() debe ser lo ultimo que se ejecute
 root.mainloop() # Mantiene la ventana abierta
